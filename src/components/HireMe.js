9c9ff0624dee3b8e0374bcb14c95ad68ca8f3f97
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-04-02 10:13:57 +0545

    last of design

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -1,20 +1,20 @@
 import Link from 'next/link'
 import React from 'react'
 
-import moon from '../../public/images/moon.png'
-
-import Image from 'next/image'
+import { CircularText } from '../components/Icons'
 
 const HireMe = () => {
   return (
     <div className='fixed left-4 bottom-4 flex items-center justify-center overflow-hidden md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute sm:right-0'>
       <div className='w-48 h-auto flex items-center justify-center relative md:w-24'>
-        <Image src={moon} className={'animate-spin-slow'} />
+        <CircularText
+          className={'fill-dark animate-spin-slow dark:fill-light'}
+        />
         <Link
           href='mailto:adhikareekshitij@gmail.com'
-          className='flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#684536] text-light
-        shadow-md border border-solid border-dark w-20 h-20 rounded-full font-semibold hover:bg-[#492c20] hover:text-light
-        dark:border-light   dark:hover:bg-[#684536]/50 dark:hover:text-light md:w-12 md:h-12 md:text-[10px] '
+          className='flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light
+          shadow-md border border-solid border-dark w-20 h-20 rounded-full font-semibold hover:bg-light hover:text-dark
+          dark:border-light   dark:hover:bg-light dark:hover:text-dark md:w-12 md:h-12 md:text-[10px]'
         >
           Connect
         </Link>
