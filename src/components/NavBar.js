9eeb83268133eca33ec4ba5d966fe688a5b77f4f
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-04-15 16:30:15 +0545

    update profile pic for xs and navbar update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -157,7 +157,7 @@ const NavBar = () => {
         <motion.div
           initial={{ scale: 0, opacity: 0, x: '-50%', y: '-50%' }}
           animate={{ scale: 1, opacity: 1 }}
-          className='min-w-[70vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/75 dark:bg-light/75 rounded-lg backdrop-blur-md py-32'
+          className='fixed min-w-[70vw] flex flex-col justify-between z-30 items-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/75 dark:bg-light/75 rounded-lg backdrop-blur-md py-[60px]'
         >
           <nav className='flex items-center flex-col justify-center '>
             <CustomMobileLink
