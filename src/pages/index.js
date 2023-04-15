db638265be2b5cf0a910036f79a9eaa23677ba1c
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-04-15 16:09:08 +0545

    resume and profile pic update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import Head from 'next/head'
 import Layout from '@/components/Layout'
 import Image from 'next/image'
-import profilePic from '../../public/images/profile/profile.png'
+import profilePic from '../../public/images/profile/profile1.png'
 import AnimatedText from '@/components/AnimatedText'
 import Link from 'next/link'
 import { LinkArrow } from '@/components/Icons'
 import HireMe from '@/components/HireMe'
-import color from '../../public/images/color.gif'
 import TransitionEffect from '@/components/TransitionEffect'
 
 function Home() {
@@ -22,20 +21,27 @@ function Home() {
         <Layout className='pt-0 md:p-16 sm:pt-2'>
           <div className='flex items-center justify-between w-full lg:flex-col'>
             <div className='w-1/2 lg:w-full md:w-full'>
-              <Image
-                src={profilePic}
-                alt='profilePic'
-                width={250}
-                height={200}
-                priority={true}
-                sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw'
-                className='ml-32 lg:inline-block md:inline-block md:w-[14rem] sm:w-[12rem] sm:mx-20 sm:mt-4'
-              />
+              <div
+                className='w-[420px] h-[490px] bg-primaryDark dark:bg-primary border-[12px] border-dark transition-[border-radius] duration-[0.5s] ease-[cubic-bezier(0.175,0.885,0.32,1.275)] rounded-[10px] 
+              shadow-[inset_0_-3em_3em_rgba(0,0,0,0.1),0_0_0_2px_rgb(190,190,190),0.3em_0.3em_1em_rgba(0,0,0,0.3)]
+              dark:shadow-[inset_0_-3em_3em_rgba(255,255,255,0.1),0_0_0_2px_rgb(53,53,53),0.3em_0.3em_1em_rgba(255,255,255,0.3)]
+              mt-24 md:w-[20rem] md:h-[341px] md:mt-16 md:ml-[72px] sm:ml-20 xs:ml-10 lg:ml-[160px] sm:mt-32 mb-4 dark:border-light'
+              >
+                <Image
+                  src={profilePic}
+                  alt='profilePic'
+                  width={400}
+                  height='auto'
+                  priority={true}
+                  sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw'
+                  className='-mt-32 lg:inline-block md:inline-block md:w-[24rem] sm:w-[20rem] '
+                />
+              </div>
             </div>
             <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
               <AnimatedText
                 text='Bringing ideas to life with programming and design.'
-                className='xl:!text-5xl lg:!text-center lg:!text-4xl md:!text-3xl sm:!text-3xl'
+                className='xl:!text-5xl lg:!text-center lg:!text-4xl md:!text-3xl sm:!text-3xl justify-center'
               />
               <p className='my-4 text-base font-medium md:text-sm sm:text-xs text-justify'>
                 As a skilled full-stack developer, I am dedicated to turning
@@ -65,17 +71,7 @@ function Home() {
             </div>
           </div>
         </Layout>
-        <HireMe/>
-        <div className='absolute right-10 -bottom-16 inline-block w-30 md:hidden'>
-          <Image
-            src={color}
-            alt='lightbulb'
-            width='140'
-            height='140'
-            className='w-[220px] h-auto '
-            
-          />
-        </div>
+        <HireMe />
       </main>
     </>
   )
