0dfe200f350c8f1cc6756142dd9a8f4b56b8829c
KshitijAdhikaree <adhikareekshitij@gmail.com>
2025-05-19 12:32:02 +0545

    added text to dark in dark mode

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,12 +4,12 @@ import React from "react";
 const Logo = () => {
   return (
     <div
-      className="
-        w-16 h-16 flex items-center justify-center rounded-2xl border-2 bg-[length:200%_100%] animate-gradient-bg"
+      className=" w-16 h-16 flex items-center justify-center rounded-2xl bg-[length:200%_100%] animate-gradient-bg transition-transform duration-100 hover:scale-105 hover:shadow-lg
+      "
     >
       <Link
         href="/"
-        className="text-3xl font-bold animate-text-color transition-transform duration-100"
+        className="text-3xl font-bold animate-text-color transition-colors duration-300 hover:text-light dark:hover:text-dark"
       >
         KA
       </Link>
