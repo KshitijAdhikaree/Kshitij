96d17e27150588c773861e8ede7ec0e1ac52dc7b
KshitijAdhikaree <adhikareekshitij@gmail.com>
2025-05-19 12:47:17 +0545

    fixed some issues on hireme

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -6,7 +6,7 @@ const HireMe = () => {
   return (
     <div className="fixed left-4 bottom-4 flex items-center justify-center overflow md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute sm:hidden">
       <div className="w-48 h-48 flex items-center justify-center relative md:w-24 md:h-24">
-        <Circle className="fill-dark  dark:fill-light  animate-ping " />
+        <Circle className="fill-dark dark:fill-light animate-ping" />
         <Link
           href="mailto:adhikareekshitij@gmail.com"
           className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light dark:text-dark dark:bg-light
