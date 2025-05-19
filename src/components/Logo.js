e66a3be9979de5c014ecbdd9363110f3ed4e2bdd
KshitijAdhikaree <adhikareekshitij@gmail.com>
2025-05-20 00:03:11 +0545

    combine logo div and link to link

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,14 +3,12 @@ import React from "react";
 
 const Logo = () => {
   return (
-    <div className="w-16 h-16 my-2 flex items-center justify-center rounded-2xl bg-gradient-logo bg-400 animate-bganimation transition-transform duration-300 hover:scale-110 shadow-dark dark:shadow-light">
-      <Link
-        href="/"
-        className="text-light dark:text-dark text-3xl font-bold dark:hover:text-light hover:text-dark"
-      >
-        KA
-      </Link>
-    </div>
+    <Link
+      href="/"
+      className="w-16 h-16 my-2 flex items-center justify-center rounded-2xl bg-gradient-logo bg-400 animate-bganimation transition-transform duration-300 hover:scale-110 shadow-dark dark:shadow-light text-light dark:text-dark text-3xl font-bold dark:hover:text-light hover:text-dark"
+    >
+      KA
+    </Link>
   );
 };
 
